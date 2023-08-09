2cd87304936bed4e0acdaef9b6b1fee8523f8328
Adalberto Junior <adalberto.gmu@gmail.com>
2023-08-09 12:07:19 -0300

    Initial

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/souzagmu/hotel-project/api"
+	"github.com/souzagmu/hotel-project/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	dburi    = "mongodb://root:example@localhost:27017"
+	dbname   = "hotel-project"
+	userColl = "users"
+)
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
+func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "Listen Address")
+	flag.Parse()
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+	app := fiber.New(config)
+	apiv1 := app.Group("/api/v1")
+
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
+	apiv1.Post("/user", userHandler.HandlePostUser)
+	app.Listen(*listenAddr)
+
+}
