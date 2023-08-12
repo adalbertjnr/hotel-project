6f991fe51455b9f9aa2ce34062a9846676918a14
Adalberto Junior <adalberto.gmu@gmail.com>
2023-08-12 16:25:46 -0300

    supd

--- types/user.go
+++ types/user.go
@@ -1,13 +1,36 @@
 package types
 
 import (
+	"fmt"
+	"regexp"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
 
 const (
-	bcryptCost = 12
+	bcryptCost      = 12
+	minFirstNameLen = 3
+	minLastNameLen  = 3
+	minPasswordLen  = 7
 )
+
+func (p UpdateUserParams) ToBSON() bson.M {
+	m := bson.M{}
+	if len(p.FirstName) > 0 {
+		m["firstName"] = p.FirstName
+	}
+	if len(p.LastName) > 0 {
+		m["lastName"] = p.LastName
+	}
+	return m
+}
+
+type UpdateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
 
 type CreateUserParams struct {
 	ID        string `json:"id,omitempty"`
@@ -15,6 +38,28 @@ type CreateUserParams struct {
 	LastName  string `json:"lastName"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
+}
+
+func (params CreateUserParams) Validate() map[string]string {
+	errors := map[string]string{}
+	if len(params.FirstName) < minFirstNameLen {
+		errors["firstName"] = fmt.Sprintf("first name length should be at least %d characters", minFirstNameLen)
+	}
+	if len(params.LastName) < minLastNameLen {
+		errors["lastName"] = fmt.Sprintf("last name length should be at least %d characters", minLastNameLen)
+	}
+	if len(params.Password) < minPasswordLen {
+		errors["password"] = fmt.Sprintf("password length should be at least %d characters", minPasswordLen)
+	}
+	if !isEmailValid(params.Email) {
+		errors["email"] = fmt.Sprintf("email is invalid")
+	}
+	return errors
+}
+
+func isEmailValid(e string) bool {
+	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	return emailRegex.MatchString(e)
 }
 
 type User struct {
