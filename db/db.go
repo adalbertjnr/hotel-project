d9f8f5238ee370cdf06f222063b27c18974c73ff
Adalberto Junior <adalberto.junior@baseservice.io>
2023-08-12 22:49:01 -0300

    supd

--- db/db.go
+++ db/db.go
@@ -1,1 +1,9 @@
 package db
+
+const (
+	COLL_ROOM  = "rooms"
+	COLL_USER  = "users"
+	COLL_HOTEL = "hotels"
+	DBNAME     = "hotel-project"
+	DBURI      = "mongodb://root:example@localhost:27017"
+)
---
+++ scripts/seed.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/souzagmu/hotel-project/db"
+	"github.com/souzagmu/hotel-project/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	ctx := context.Background()
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
+	roomStore := db.NewMongoRoomStore(client, db.DBNAME)
+
+	hotel := types.Hotel{
+		Name:     "Bellucia",
+		Location: "France",
+	}
+
+	rooms := []types.Room{
+		{
+			Type:      types.SingleRoomType,
+			BasePrice: 99.9,
+		},
+		{
+			Type:      types.DeluxeRoomType,
+			BasePrice: 199.9,
+		},
+		{
+			Type:      types.SeaSideRoomType,
+			BasePrice: 122.9,
+		},
+	}
+
+	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, room := range rooms {
+		room.HotelID = insertedHotel.ID
+		insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("insertedRoom:", insertedRoom)
+	}
+}
