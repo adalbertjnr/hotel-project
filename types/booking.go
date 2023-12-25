0e46406149805bf287c277fb8ae0b4ac333eb426
Adalberto Junior <adalberto.junior@baseservice.io>
2023-12-25 18:40:02 -0300

    booking type

---
+++ types/booking.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Booking struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID   primitive.ObjectID `bson:"userID,omitempty" json:"userID,omitempty"`
+	RoomID   primitive.ObjectID `bson:"roomID,omitempty" json:"roomID,omitempty"`
+	FromDate time.Time          `bson:"fromDate,omitempty" json:"fromDate,omitempty"`
+	Tilldate time.Time          `bson:"tillDate" json:"tillDate,omitempty"`
+}
