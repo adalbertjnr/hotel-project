b7f6bb1b612d49ed556ac93c93de09eb99cabee2
Adalberto Junior <adalberto.junior@baseservice.io>
2023-12-26 17:17:22 -0300

    booking type

--- types/booking.go
+++ types/booking.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Booking struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserID   primitive.ObjectID `bson:"userID,omitempty" json:"userID,omitempty"`
-	RoomID   primitive.ObjectID `bson:"roomID,omitempty" json:"roomID,omitempty"`
-	FromDate time.Time          `bson:"fromDate,omitempty" json:"fromDate,omitempty"`
-	Tilldate time.Time          `bson:"tillDate" json:"tillDate,omitempty"`
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID     primitive.ObjectID `bson:"userID,omitempty" json:"userID,omitempty"`
+	RoomID     primitive.ObjectID `bson:"roomID,omitempty" json:"roomID,omitempty"`
+	NumPersons int                `bson:"numPersons,omitempty" json:"numPersons,omitempty"`
+	FromDate   time.Time          `bson:"fromDate,omitempty" json:"fromDate,omitempty"`
+	Tilldate   time.Time          `bson:"tillDate" json:"tillDate,omitempty"`
 }
