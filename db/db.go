d954aead21517980f827fe1627935fb1357a2b5b
Adalberto Junior <adalberto.gmu@gmail.com>
2023-12-26 17:16:57 -0300

    add bookingStorer to db.store struct

--- db/db.go
+++ db/db.go
@@ -1,15 +1,17 @@
 package db
 
 const (
-	COLL_ROOM  = "rooms"
-	COLL_USER  = "users"
-	COLL_HOTEL = "hotels"
-	DBNAME     = "hotel-project"
-	DBURI      = "mongodb://root:example@localhost:27017"
+	COLL_ROOM     = "rooms"
+	COLL_USER     = "users"
+	COLL_HOTEL    = "hotels"
+	COLL_BOOKINGS = "bookings"
+	DBNAME        = "hotel-project"
+	DBURI         = "mongodb://root:example@localhost:27017"
 )
 
 type Store struct {
-	User  UserStorer
-	Hotel HotelStorer
-	Room  RoomStorer
+	User    UserStorer
+	Hotel   HotelStorer
+	Room    RoomStorer
+	Booking BookingStorer
 }
