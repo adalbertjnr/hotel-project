4b93f255f4e23c561e28abe3c4cd5f11b766c907
Adalberto Junior <adalberto.junior@baseservice.io>
2023-12-26 20:51:13 -0300

    get rooms handler

--- main.go
+++ main.go
@@ -76,6 +76,7 @@ func main() {
 	apiv1.Get("/hotel/:id", hotelHandler.HandleGetHotel)
 	apiv1.Get("/hotel/:id/rooms", hotelHandler.HandleGetRooms)
 
+	apiv1.Get("/room", roomHandler.HandleGetRooms)
 	apiv1.Post("/room/:id/book", roomHandler.HandleBookRoom)
 
 	log.Fatal(app.Listen(*listenAddr))
