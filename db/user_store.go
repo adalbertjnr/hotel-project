0b51f225fd6e7a26d32e42e6c6327f463c640edb
Adalberto Junior <adalberto.junior@baseservice.io>
2024-01-01 10:12:55 -0300

    small refactoring from bsonD to bsonM

--- db/user_store.go
+++ db/user_store.go
@@ -50,10 +50,7 @@ func (s *MongoUserStore) GetUserByEmail(ctx context.Context, email string) (*typ
 }
 
 func (s *MongoUserStore) UpdateUser(ctx context.Context, filter bson.M, params types.UpdateUserParams) error {
-	update := bson.D{
-		{Key: "$set", Value: params.ToBSON()},
-	}
-	fmt.Println("Printing toBSON func", params.ToBSON())
+	update := bson.M{"$set": params}
 	_, err := s.coll.UpdateOne(ctx, filter, update)
 	if err != nil {
 		return err
